1f7375074b78840d77eb9f1ee55c98b7eb56a75b
cfworx <p2@cfworx.tech>
2026-08-25 06:19:35 -0700

    Added latest CCNA notes from Cisco U

--- restart-both-gateways.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "H=%LOCALAPPDATA%\hermes\hermes-agent\venv\Scripts\hermes.exe"
-echo ==== restart run %date% %time% ==== > "%LOCALAPPDATA%\hermes\restart-log.txt"
-call "%H%" gateway restart >> "%LOCALAPPDATA%\hermes\restart-log.txt" 2>&1
-call "%H%" -p ashley gateway restart >> "%LOCALAPPDATA%\hermes\restart-log.txt" 2>&1
-call "%H%" gateway status >> "%LOCALAPPDATA%\hermes\restart-log.txt" 2>&1
-exit
